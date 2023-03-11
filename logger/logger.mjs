fb42d77188da9a086174750953a3301015f6113c
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-03-11 19:51:04 +0100

    remove logger label

--- logger/logger.mjs
+++ logger/logger.mjs
@@ -1,20 +1,17 @@
 import { createLogger, format, transports } from "winston";
+
 const { combine, timestamp, label, prettyPrint } = format;
-import dayjs from "dayjs";
 
 const logger = createLogger({
   level: "info",
-    format: combine(
-        label({ label: `${dayjs()}` }),
-        prettyPrint()
-    ),
+  format: combine(prettyPrint()),
   defaultMeta: { service: "news-in-images" },
   transports: [new transports.File({ filename: "./logs/logs.log" })],
 });
 
 logger.add(
   new transports.Console({
-    format: format.combine(format.colorize(), format.simple()),
+    format: format.combine(format.colorize(), format.prettyPrint()),
   })
 );
 
