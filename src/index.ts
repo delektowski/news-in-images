f727dedd11f11702cd8263a7fc3e0fccf765b6f7
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-03-18 21:48:48 +0100

    add env for public and views

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ import { handleSaveNewsImages } from "./db/handleSaveNewsImages";
 import { handleTableCreation } from "./db/database";
 import * as path from 'path';
 
-(global as any).__basedir = __dirname;
+const devPublicPath = "./src/public";
+const prodPublicPath = "../src/public";
+const devViewsPath = "./src/views";
+const prodViewsPath = "../src/views";
 
 const app: Express = express();
 const PORT = 5555;
 handleTableCreation();
 handleSaveNewsImages();
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(process.cwd(),
+    `${process.env.NODE_ENV==="production" ? prodPublicPath : devPublicPath}`)));
 app.use(express.static("node_modules/flag-icons"));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(process.cwd(),
+    `${process.env.NODE_ENV==="production" ? prodViewsPath : devViewsPath}`));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
