737f30393b1dddf1e0c382de36652bfad4237f11
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-03-19 19:50:11 +0100

    add env for logger

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,12 +1,17 @@
 import { createLogger, format, transports } from "winston";
+import path from "path";
 
 const { combine, timestamp, label, prettyPrint } = format;
+
+const devLoggerPath = "./src/logs/logs.log";
+const prodLoggerPath = "../src/logs/logs.log";
 
 const logger = createLogger({
   level: "info",
   format: combine(prettyPrint()),
   defaultMeta: { service: "news-in-images" },
-  transports: [new transports.File({ filename: "./logs/logs.log" })],
+  transports: [new transports.File({ filename: path.join(process.cwd(),
+        `${process.env.NODE_ENV==="production" ? prodLoggerPath : devLoggerPath}`) })],
 });
 
 logger.add(
