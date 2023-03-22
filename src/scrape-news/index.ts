c803ae5b0d61b3b51ea412130faffa9bc8fdbb77
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-03-22 22:16:58 +0100

    fix chromium path

--- src/scrape-news/index.ts
+++ src/scrape-news/index.ts
@@ -1,17 +1,7 @@
-import playwright from "playwright";
-import {
-  dataARABNEWS,
-  dataCHINADAILY,
-  dataDEUTSCHEWELLE, dataHINDUSTANTIMES,
-  dataLEMONDE,
-  dataNYT,
-  dataPAP,
-  dataTASS,
-  dataTELEGRAPH,
-} from "./news-providers";
+import { chromium } from "playwright";
+import { dataLEMONDE, dataNYT, dataTELEGRAPH } from "./news-providers";
 import logger from "../logger/logger";
-import dayjs from "dayjs";
-import {NewsTitlesModel} from "./models/news-titles.model";
+import { NewsTitlesModel } from "./models/news-titles.model";
 
 export async function getNewsTitles() {
   const targetsData = [
@@ -25,8 +15,8 @@ export async function getNewsTitles() {
     // dataHINDUSTANTIMES,
     // dataARABNEWS
   ];
-  const newsTitles:NewsTitlesModel[] = [];
-  const browser = await playwright.chromium.launch();
+  const newsTitles: NewsTitlesModel[] = [];
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   for (const item of targetsData) {
     const targetData = {
@@ -38,13 +28,16 @@ export async function getNewsTitles() {
     await page.goto(item.url);
     const newsData = await page.$$eval(
       item.mainSelector,
-      async (element, { subSelector,linkSelector, newsProvider, country }) => {
-        const data:NewsTitlesModel[] = [];
+      async (element, { subSelector, linkSelector, newsProvider, country }) => {
+        const data: NewsTitlesModel[] = [];
         await element.forEach((item, index) => {
           if (index === 0) {
-            const title:string = (item.querySelector(subSelector) as HTMLElement)?.innerText;
-            const link = (item.querySelector(linkSelector) as HTMLAnchorElement)?.href;
-            title && data.push({ title, newsProvider, country,link });
+            const title: string = (
+              item.querySelector(subSelector) as HTMLElement
+            )?.innerText;
+            const link = (item.querySelector(linkSelector) as HTMLAnchorElement)
+              ?.href;
+            title && data.push({ title, newsProvider, country, link });
           }
         });
         return data;
@@ -59,5 +52,3 @@ export async function getNewsTitles() {
   });
   return newsTitles;
 }
-
-
