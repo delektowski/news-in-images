e4cfaca4bcce91a1bd4b4f89ccb9a8ca758ea638
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-09-11 23:46:02 +0200

    add remove special characters form the titles

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,9 +1,14 @@
 import * as dayjs from "dayjs";
 import logger from "../logger/logger";
-import {NewsSelectorDataModel} from "../models/newsSelectorData.model";
+import { NewsSelectorDataModel } from "../models/newsSelectorData.model";
 
 export function generateTitle(prompt: string): string {
-  return prompt.split(" ").splice(0, 5).join("-").toLowerCase();
+  return prompt
+    .replace(/[^a-zA-Z0-9\s\-/]/g, '')
+    .split(" ")
+    .splice(0, 5)
+    .join("-")
+    .toLowerCase();
 }
 
 export const currentDate = (): string => dayjs().format("YYYY-MM-DD");
@@ -11,7 +16,6 @@ export const currentDate = (): string => dayjs().format("YYYY-MM-DD");
 export const isBeforeHour = (): boolean => dayjs().hour() < 8;
 
 export const currentNewsDate = (): string => {
-
   if (isBeforeHour()) {
     return dayjs().subtract(1, "day").format("YYYY-MM-DD");
   }
@@ -26,11 +30,11 @@ export const formatMonthToString = (date: string): string => {
   return dayjs(date).format("D MMMM YYYY");
 };
 
-export const shuffleArrayOrder = (arr: NewsSelectorDataModel[]): NewsSelectorDataModel[] => {
+export const shuffleArrayOrder = (
+  arr: NewsSelectorDataModel[]
+): NewsSelectorDataModel[] => {
   return arr
     .map((value: NewsSelectorDataModel) => ({ value, sort: Math.random() }))
     .sort((a: { sort: number }, b: { sort: number }) => a.sort - b.sort)
     .map(({ value }) => value);
-}
-
-
+};
